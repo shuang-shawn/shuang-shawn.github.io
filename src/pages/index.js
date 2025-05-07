f8c0832bb35f4a3a81452a5e36096a31879b9a24
Shuang Wu <shuang111994@gmail.com>
2025-05-07 16:27:54 -0700

    added two projects, cleaned up format

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,8 @@ export default function IndexPage() {
         {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
         <AboutSection sectionId="about" heading="About Me" />
         <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <ProjectsSection sectionId="features" heading="Projects" />
+        <ContactSection sectionId="github" heading="Contact Me" />
       </Page>
     </>
   );
